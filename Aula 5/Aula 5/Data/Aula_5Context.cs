38e2abc26edf4f7d65bbc5b59e39269408df15bd
Vitor Ribeiro <129668935+VitorARibeiro@users.noreply.github.com>
2024-10-15 12:26:44 +0100

    up

---
+++ Aula 5/Aula 5/Data/Aula_5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Aula_5.Models;
+
+namespace Aula_5.Data
+{
+    public class Aula_5Context : DbContext
+    {
+        public Aula_5Context (DbContextOptions<Aula_5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aula_5.Models.Books> Books { get; set; } = default!;
+    }
+}
---
+++ Aula 5/Aula 5/Models/BookViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aula_5.Models
+{
+    public class BookViewModel
+    {
+        [Required(ErrorMessage = "Required Field")]
+        [StringLength(100, ErrorMessage = "The {0} do not exceed {1} characteres.")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Select a Image File")]
+        public IFormFile? CoverPhoto { get; set; }
+
+        [Required(ErrorMessage = "Select a Document_ File")]
+        public IFormFile? Document { get; set; }
+    }
+}
